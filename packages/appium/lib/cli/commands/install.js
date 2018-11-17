06b330dcfec7821af3439c2f6b82c828fb6f17c4
dpgraham <dpgraham.85@gmail.com>
2018-11-17 15:37:53 -0800

    Move install logic to api/

--- packages/appium/lib/cli/commands/install.js
+++ packages/appium/lib/cli/commands/install.js
@@ -1,38 +1,31 @@
-import { Command } from '@oclif/command';
-import { exec } from 'teen_process';
-import { fs } from 'appium-support';
-import path from 'path';
+import { Command, flags as Flags } from '@oclif/command';
+import { install } from '../api';
 
 class Install extends Command {
   async run () {
-    const {args} = this.parse(Install);
+    const {args, flags} = this.parse(Install);
     const {package: pkg} = args;
-    this.log(`Installing package "${pkg}"`);
 
-    // TODO: Temporary. This logic needs to be moved to an API
-    const appiumDriversPath = path.resolve('appium-drivers');
-    const driversJsonPath = path.resolve(appiumDriversPath, 'drivers.json');
-    if (!await fs.exists(driversJsonPath)) {
-      await fs.writeFile(driversJsonPath, JSON.stringify({}), "utf8");
-    }
-    const packageJsonPath = path.resolve(appiumDriversPath, 'package.json');
-    if (!await fs.exists()) {
-      const packageJsonBase = path.resolve(appiumDriversPath, 'package-base.json');
-      await fs.copyFile(packageJsonBase, packageJsonPath);
-    }
-    const installedDrivers = require(driversJsonPath);
-    installedDrivers[pkg] = pkg;
-    await fs.writeFile(driversJsonPath, JSON.stringify(installedDrivers));
-    await exec('yarn', ['add', args.package, '--production'], {cwd: appiumDriversPath});
+    let source;
+    if (flags.git) { source = 'git'; }
+    if (flags.npm) { source = 'npm'; }
+    if (flags.file) { source = 'file'; }
+
+    this.log(`Installing Appium driver: "${pkg}"${source ? ` from source ${source}` : ""}`);
+    await install(args.package, source);
+    this.log(`Successfully installed Appium Driver: "${args.package}"`);
   }
 }
 
-Install.description = `Describe the command here
+Install.description = `Installs an Appium driver
 ...
 Extra documentation goes here
 `;
 
 Install.flags = {
+  git: Flags.boolean({description: 'Install an Appium driver from a git repository'}),
+  npm: Flags.boolean({description: 'Install an Appium driver from an NPM repository'}),
+  file: Flags.boolean({description: 'Install an Appium driver from a local directory'}),
 };
 
 Install.args = [
